ff773c91418d4214f4666c0d8e03754857d7cf95
Marco Lara <marek_lara@hotmail.com>
2025-04-13 12:30:28 -0500

    Update ProjectsMain component and fix alignments

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -15,9 +15,9 @@ const HeroPic = () => {
       className="h-full flex items-center justify-center overflow-hidden"
     >
       <img src={frontend} alt="Hero pic" className="max-h-[450px] w-auto" />
-      <div className="absolute -z-10 flex justify-center items-center animate-pulse overflow-hidden">
+      {/* <div className="absolute -z-10 flex justify-center items-center animate-pulse overflow-hidden">
         <LuLoaderCircle className="md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
-      </div>
+      </div> */}
     </motion.div>
   );
 };
